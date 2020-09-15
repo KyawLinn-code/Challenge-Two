a0038c69165f8e6826efc95d23fda547c221d649
kyawlinn-code <klin54369@gmail.com>
2020-09-15 14:12:41 +0630

    changed image file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function generateImage(){
     var div = document.getElementById('img-container');
     var img = document.createElement('img');
-    img.src = "test.jpg";
+    img.src = "https://media2.giphy.com/media/l3q2RJBdaqJY2SV3O/giphy.gif";
     img.classList.add('rounded');
     img.classList.add('float-left');
     div.appendChild(img);
